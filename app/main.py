459eaf451a6570d9b851a9734a867a5467e72a61
Ander <ander.aramburu@nextdigital.es>
2025-03-11 17:49:09 +0100

    feat: initialize practice removing solutions

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-
 from app.application.get_dice_roll import get_dice_roll
+from fastapi.responses import RedirectResponse
 
 
 def create_app() -> FastAPI:
+    my_unused_variable = 1
+
     app = FastAPI()
 
-    app.add_api_route(path="/", endpoint=lambda: RedirectResponse(app.url_path_for(get_dice_roll.__name__)), methods=["GET"], include_in_schema=False)
-
     app.add_api_route(
-        path="/dice/roll",
-        endpoint=get_dice_roll,
+        path="/",
+        endpoint=lambda: RedirectResponse(app.url_path_for(get_dice_roll.__name__)),
+        methods=["GET"],
+        include_in_schema=False,
     )
+
+    app.add_api_route(path="/dice/roll", endpoint=get_dice_roll)
 
     return app
 
